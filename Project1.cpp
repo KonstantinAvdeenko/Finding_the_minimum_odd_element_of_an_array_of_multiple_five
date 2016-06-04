6e9fce4da18250b849e9fff19a12d9cd9174a53c
KonstantinAvdeenko <AvdeenkoKonstantin1995@gmail.com>
2016-06-04 20:55:27 +0400

    Add my finding the minimum odd element of an array of multiple five

---
+++ Project1.cpp
@@ -0,0 +1,43 @@
+﻿#include <tchar.h>
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+int _tmain(int argc, wchar_t argv[]) // функция для работы с символами юникода и нет
+{
+	srand(time(NULL)); // генератор псевдослучайных чисел основывающийся на временную дату
+	setlocale(LC_ALL, "Russian"); // поддержка работы с русскими символами 
+	int arr[20];
+	for (int i = 0; i < 20; i++)
+	{
+		arr[i] = rand() % 2000 - 1000;
+		cout << " " << arr[i]; // заполнение и вывод элементов массива с рандомными числами от -1000 до 1000 
+	}
+	int min = 2000;
+	for (int j = 0; j<20; j++)
+	{
+		if (arr[j] % 2 != 0)
+		{
+			if (arr[j] % 5 == 0)
+			{ 
+				if (arr[j]<min)
+				{ 
+					min = arr[j]; 
+				} 
+			}
+		}
+	}
+	if (min == 2000)
+	{
+		cout << endl;
+		cout << "Нет элемента, удовлетворяющего условию";
+	}
+	else
+	{
+		cout << endl;
+		cout << "Минимальный нечетный элемент массива кратный 5 равен " << min;
+	}
+	cin.get(); // окончание работы с потоками
+	return 0;
+}
---
+++ READ_ME_ENG.md
@@ -0,0 +1,4 @@
+﻿This program in the C++ programming language finds the minimum odd element of an array multiple of five.
+The result is output through the output stream.
+The main module Project1 contains a single main function:
+_tmain - fully performing the above task.
